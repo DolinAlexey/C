dab1e36ea3eeff5323de48fe41df58870804f934
DolinAlexey <dolin.alexey@gmail.com>
2020-03-14 16:46:42 +0300

    Add files via upload
    Записать конструкцию, с помощью которой десять раз повторится оператор
    printf("Hello World\n") ;

---
+++ main_repeat_ten_times.c
@@ -0,0 +1,18 @@
+//  Записать конструкцию, с помощью которой десять раз повторится оператор
+//  printf("Hello World\n") ;
+//
+//  Created by DOLIN Aleksey on 14.03.2020.
+//  Copyright © 2020 Dolin Aleksey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    for (i = 0; i < 10; i++)
+    {
+        printf("Hello, World!\n");
+    }
+    return 0;
+}
